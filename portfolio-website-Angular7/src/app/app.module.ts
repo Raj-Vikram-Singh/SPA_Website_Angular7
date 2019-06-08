f5960d667054867192173861578c18f9c7e495a8
RAJ VIKRAM SINGH <38528859+Raj-Vikram-Singh@users.noreply.github.com>
2019-06-08 21:13:40 +0530

    component created

--- portfolio-website-Angular7/src/app/app.module.ts
+++ portfolio-website-Angular7/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { OurServicesComponent } from './our-services/our-services.component';
+import { ThankyouComponent } from './thankyou/thankyou.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    FeedbackComponent,
+    OurServicesComponent,
+    ThankyouComponent
   ],
   imports: [
     BrowserModule,
